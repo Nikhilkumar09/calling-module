c56da38c361323f836cae737a9fcbfe3efbb5233
Nikhilkumar09 <66530687+Nikhilkumar09@users.noreply.github.com>
2026-03-15 18:59:08 -0400

    Update rootmodule.tf

--- rootmodule.tf
+++ rootmodule.tf
@@ -1,10 +1,10 @@
-module "STRING_MODULE" {
+module "ROOT_MODULE" {
   source  = "app.terraform.io/FALCON_ORG/STRING_MODULE/random"
   version = "1.0.0"
 }
 
 output "example" {
 
-value = module.STRING_MODULE.string
+value = module.ROOT_MODULE.string
 
 }
